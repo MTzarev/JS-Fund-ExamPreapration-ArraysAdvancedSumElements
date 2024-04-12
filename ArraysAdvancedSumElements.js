4cf93771e7ec9e6924e2dbe8b8b0330485225df7
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-12 21:14:15 +0300

    Update ArraysAdvancedSumElements.js

--- ArraysAdvancedSumElements.js
+++ ArraysAdvancedSumElements.js
@@ -2,7 +2,6 @@ function arraysAdvancedSumElements(input){
 let firstEl = Number(input.shift());
 let lastEl = Number(input.pop());
 let sum = ((firstEl+lastEl))
-
     console.log(sum);
 }
 arraysAdvancedSumElements(['20', '30', '40']);
